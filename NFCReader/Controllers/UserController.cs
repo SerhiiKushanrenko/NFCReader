3a4d40fb59bfa8f7fbd75284eece04c9f97524a5
SerhiiKushanrenko <Serhii.Kushnarenko@fivesysdev.com>
2022-11-29 13:25:19 +0200

    Add project files.

---
+++ NFCReader/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NFCReader.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public int GetGuid()
+        {
+            var a = 15;
+            return a;
+        }
+    }
+}
