b09252284d24044c1199db50a2bad7b86c97f548
SerhiiKushanrenko <Serhii.Kushnarenko@fivesysdev.com>
2022-12-14 13:08:26 +0200

    add Tests

---
+++ NfcReader.UnitTests/HttpServiceTest.cs
@@ -0,0 +1,66 @@
+﻿using BLL.DTO;
+using BLL.Services;
+using Moq;
+using Moq.Protected;
+using System.Net;
+
+namespace NfcReader.UnitTests
+{
+    public class HttpServiceTest
+    {
+        public static IEnumerable<object[]> GetUserAuthDTO()
+        {
+
+            yield return new object[]
+            {
+                new UserAuthDTO()
+                {
+                    UsbDeviceId = "null",
+                    DeviceId = Guid.NewGuid(),
+                    Name = "Sam"
+                },HttpStatusCode.OK,
+            };
+
+            yield return new object[]
+            {
+                new UserAuthDTO()
+                {
+                    UsbDeviceId = "null",
+                    DeviceId = Guid.NewGuid(),
+                },HttpStatusCode.BadRequest,
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetUserAuthDTO))]
+        public async void ShouldCallCreatePostApi(UserAuthDTO userAuthDTO, HttpStatusCode httpStatusCode)
+        {
+            var handlerMock = new Mock<HttpMessageHandler>();
+            var response = new HttpResponseMessage
+            {
+                StatusCode = httpStatusCode
+            };
+            handlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(response);
+            var httpClient = new HttpClient(handlerMock.Object);
+            var httpService = new HttpService(httpClient);
+
+            var statusCode = await httpService.CallCloudBackEnd(userAuthDTO);
+
+            handlerMock.Protected().Verify(
+                "SendAsync",
+                Times.Exactly(1),
+                ItExpr.Is<HttpRequestMessage>(req => req.Method == HttpMethod.Post),
+                ItExpr.IsAny<CancellationToken>());
+
+            Assert.Equal(httpStatusCode, statusCode);
+
+
+        }
+    }
+}
---
+++ NfcReader.UnitTests/UsbListenerTest.cs
@@ -0,0 +1,42 @@
+using AutoFixture;
+using BLL.DTO;
+using BLL.Services;
+using BLL.Services.Interfaces;
+using Moq;
+
+namespace NfcReader.UnitTests
+{
+    public class UsbListenerTest
+    {
+        private readonly Mock<IUsbDeviceService> _usbDeviceServiceMock;
+        public List<UsbDeviceInfoDTO> _staticDevices = new List<UsbDeviceInfoDTO>()
+        {
+            new UsbDeviceInfoDTO(new Fixture().Create<string>(),new Fixture().Create<string>(),new Fixture().Create<string>()),
+            new UsbDeviceInfoDTO(new Fixture().Create<string>(),new Fixture().Create<string>(),new Fixture().Create<string>()),
+            new UsbDeviceInfoDTO(new Fixture().Create<string>(),new Fixture().Create<string>(),new Fixture().Create<string>()),
+        };
+        public UsbListenerTest()
+        {
+            _usbDeviceServiceMock = new();
+            _usbDeviceServiceMock.Setup(b => b.StaticDevices).Returns(_staticDevices);
+        }
+
+
+        [Fact]
+        public void GetEmptyDTO_NoUsbDevicesEnter()
+        {
+            _usbDeviceServiceMock.Setup(builder => builder.GetUSBDevices()).Returns(_staticDevices);
+
+            // arrange    
+            var service = _usbDeviceServiceMock.Object;
+            var usbListener = new UsbListenerService(service);
+            var newDevices = usbListener.StartListen();
+
+            //act
+            var result = new UsbDeviceInfoDTO();
+
+            //assert
+            Assert.Equal(result.DeviceId, newDevices.DeviceId);
+        }
+    }
+}
