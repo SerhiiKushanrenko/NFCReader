d2ff2268cda50f77d4cdabafea38e64f255bd256
SerhiiKushanrenko <Serhii.Kushnarenko@fivesysdev.com>
2022-12-14 13:08:26 +0200

    add Tests

---
+++ NfcReader.UnitTests/UsbDeviceInfoTest.cs
@@ -0,0 +1,21 @@
+﻿using BLL.DTO;
+
+namespace NfcReader.UnitTests
+{
+    public class UsbDeviceInfoTest
+    {
+
+        [Fact]
+        public void GetUniqueDeviceID_True()
+        {
+            //arrange
+            var model = new UsbDeviceInfoDTO("USB\\VID_13FE&PID_4300\\071C1CB41BA41291", "dasdas", "dadasd");
+            var expect = "071C1CB41BA41291";
+
+            //act
+            var result = model.GetUniqueDeviceID();
+            //assert
+            Assert.Equal(expect, result);
+        }
+    }
+}
